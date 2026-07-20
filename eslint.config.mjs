8834179ca7b62cf02a8e61ca6208c30c619721f7
Darek Mazur <dev@darekmazur.it>
2026-07-20 02:01:11 +0200

    Linter config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,52 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { fixupPluginRules } from "@eslint/compat";
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default [
+  {
+    ignores: [
+      "**/node_modules/*",
+      "**/.next/*",
+      "**/dist/*",
+      "**/build/*",
+      "*.config.js",
+      "*.config.mjs"
+    ],
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      react: fixupPluginRules(reactPlugin),
+      "react-hooks": fixupPluginRules(reactHooksPlugin),
+      import: fixupPluginRules(importPlugin),
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      "no-shadow": "off",
+      "arrow-body-style": "off",
+      "curly": ["error", "multi"],
+      "import/extensions": "off",
+      "react/button-has-type": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": "off",
+      "import/prefer-default-export": "warn",
+      "semi": "off",
+      "react/function-component-definition": "off",
+      "indent": "off",
+      "max-len": "off",
+      "comma-dangle": "off",
+      "no-redeclare": "off",
+      "react/jsx-indent": "off",
+      "no-unused-vars": "off",
+      "operator-linebreak": "off",
+      "object-curly-newline": "off",
+      "function-paren-newline": "off",
+      "implicit-arrow-newline": "off",
+      "react/require-default-props": "off",
+      "nonblock-statement-body-position": "off",
+      "react/jsx-one-expression-per-line": "off"
+    },
+  },
+];
