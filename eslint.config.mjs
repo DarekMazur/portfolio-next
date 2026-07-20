1aa90bb47fcd8549f2f2603a870f74a162c03db7
Darek Mazur <dev@darekmazur.it>
2026-07-20 02:14:12 +0200

    Add ts plugin to linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import reactPlugin from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 import nextPlugin from '@next/eslint-plugin-next'
 import prettierConfig from 'eslint-config-prettier'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default [
   {
@@ -19,7 +21,16 @@ export default [
   },
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       react: fixupPluginRules(reactPlugin),
       'react-hooks': fixupPluginRules(reactHooksPlugin),
       import: fixupPluginRules(importPlugin),
@@ -29,6 +40,7 @@ export default [
     rules: {
       ...prettierConfig.rules,
       'prettier/prettier': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
       'no-shadow': 'off',
       'arrow-body-style': 'off',
       curly: ['error', 'multi'],
